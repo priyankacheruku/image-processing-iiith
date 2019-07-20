b11407b72ca56d611372c782f714c7cb05750ce1
priyankacheruku <priyankacheruku2016@gmail.com>
2019-07-20 13:38:49 +0530

    image selection

--- SRIP/Codes/script.js
+++ SRIP/Codes/script.js
@@ -1,18 +1,23 @@
+var checkedValue;
 function openCity(evt,choice) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+  if(checkedValue){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(choice).style.display = "block";
+    evt.currentTarget.className += " active";
   }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  else{
+    alert("select image first");
   }
-  document.getElementById(choice).style.display = "block";
-  evt.currentTarget.className += " active";
 }
-
-function slider(argument) {
+function load(argument) {
   
   var x_scale = document.getElementById("x_scale");
   var x_scale_output = document.getElementById("x_scale_value");
@@ -41,6 +46,88 @@ function slider(argument) {
 
   y_rotate_slider.oninput = function() {
     y_rotate_output.innerHTML = this.value;
+  }
+
+
+
+  // the selector will match all input controls of type :checkbox
+  // and attach a click event handler 
+  $("input:checkbox").on('click', function() {
+    // in the handler, 'this' refers to the box clicked on
+    var $box = $(this);
+    if ($box.is(":checked")) {
+      // the name of the box is retrieved using the .attr() method
+      // as it is assumed and expected to be immutable
+      var group = "input:checkbox[name='" + $box.attr("name") + "']";
+      // the checked state of the group/box on the other hand will change
+      // and the current value is retrieved using .prop() method
+      $(group).prop("checked", false);
+      $box.prop("checked", true);
+    } 
+    else {
+      $box.prop("checked", false);
+    }
+  });
+
+}//load close
+    
+function getImageId(argument) {
+
+  checkedValue = $('.radio:checked').val();
+  if(checkedValue){
+     $(document).ready(function(){
+        $('#select_image').modal('hide');
+        document.getElementById('input_image').src = "img"+checkedValue+".jpeg";
+
+     });
+  }
+  else{
+    alert("Select one image");
+  }
 }
 
+function reset(argument) {
+  var c=confirm("will reset all values");
+  if(c){
+    location.reload(true);   
+  }
 }
+
+$(document).ready(function(){
+
+  $("input[name='input_radio']").click(function(){
+    var radioValue = $("input[name='input_radio']:checked").val();  
+    if (radioValue==1) {
+      $('input[name="Scaling"]').removeAttr('disabled');
+      $(".wrap1").css('opacity', '1');
+      $('input[name="Rotation"]').attr('disabled', 'disabled');
+      $(".wrap2").css('opacity', '.2');
+      $('input[name="Translation"]').attr('disabled', 'disabled');
+      $(".wrap3").css('opacity', '.2');
+    }
+ 
+    else if (radioValue==2) {
+      $('input[name="Scaling"]').attr('disabled', 'disabled');
+      $(".wrap1").css('opacity', '.2');
+      $('input[name="Rotation"]').removeAttr('disabled');
+      $(".wrap2").css('opacity', '1');
+      $('input[name="Translation"]').attr('disabled', 'disabled');
+      $(".wrap3").css('opacity', '.2');
+
+    }
+    else if(radioValue==3){
+      $('input[name="Scaling"]').attr('disabled', 'disabled');
+      $(".wrap1").css('opacity', '.2');
+      $('input[name="Rotation"]').attr('disabled', 'disabled');
+      $(".wrap2").css('opacity', '.2');
+      $('input[name="Translation"]').removeAttr('disabled');
+      $(".wrap3").css('opacity', '1');
+
+    }
+  });
+
+  $("input[name='Scaling']").click(function(){
+    $('input[name="Scaling_value"]').removeAttr('disabled');
+      
+  });
+});
